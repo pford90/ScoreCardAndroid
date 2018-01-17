2d141898e5784ebe577b4fb320b278c6eac7ba48
Peter Ford <pford90@gmail.com>
2018-01-17 17:12:51 -0500

    Main Changes to AppBar

--- app/src/main/java/com/peterford/scorecardandroid/MainActivity.java
+++ app/src/main/java/com/peterford/scorecardandroid/MainActivity.java
@@ -2,6 +2,9 @@ package com.peterford.scorecardandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
